6c10ced3540876bd1d509b17e3e065faf4669c66
Bonnie <Bonnie@128.4.97.198>
2016-12-11 22:47:06 -0500

    initial commit

--- RocketDAL/src/main/java/rocketBase/RateDAL.java
+++ RocketDAL/src/main/java/rocketBase/RateDAL.java
@@ -23,13 +23,12 @@ public class RateDAL {
 		ArrayList<RateDomainModel> alRates = new ArrayList<RateDomainModel>();		
 		
 		try {
-			tx = session.beginTransaction();	
-			
-			//TODO - RocketDALRateDAL.getAllRates
+			tx = session.beginTransaction();
+			//			RocketDALRateDAL.getAllRates
 			//			probably not a bad idea to sort the results...  Add an OrderBy
 			//			example can be found here:
 			//  		http://www.tutorialspoint.com/hibernate/hibernate_query_language.htm			
-			List lstRates = session.createQuery("FROM RateDomainModel").list();
+			List lstRates = session.createQuery("FROM RateDomainModel R ORDER BY R.interestrate DESC").list();
 			for (Iterator iterator = lstRates.iterator(); iterator.hasNext();) {
 				RateDomainModel rte = (RateDomainModel) iterator.next();
 				alRates.add(rte);
